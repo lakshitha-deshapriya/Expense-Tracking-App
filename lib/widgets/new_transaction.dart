97f468c3bb95b55cec307eeeb96640f45ab566fe
lakshitha1993 <lakshitha_deshapriya@yahoo.com>
2020-04-13 14:20:57 +0530

    Add new transaction validations

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -5,8 +5,18 @@ class NewTransaction extends StatelessWidget {
   final amountController = TextEditingController();
   final Function addTrans;
 
-  NewTransaction( this.addTrans );
+  NewTransaction(this.addTrans);
 
+  void addNewTransaction() {
+    if (titleController.text.isEmpty ||
+        double.parse(amountController.text) <= 0) {
+          print('came');
+      return;
+    }
+    addTrans(titleController.text, double.parse(amountController.text));
+  }
+
+  //numbwe is not working for ios. Need to use numberWithOptions(decomal:true)
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -19,17 +29,18 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Title'),
               controller: titleController,
+              onSubmitted: (_) => addNewTransaction(),
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => addNewTransaction(),
             ),
             FlatButton(
               child: Text('Add Transaction'),
               textColor: Colors.purple,
-              onPressed: () {
-                addTrans( titleController.text, double.parse(amountController.text) );
-              },
+              onPressed: addNewTransaction,
             )
           ],
         ),
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -9,43 +9,48 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((trans) {
-        return Card(
-          elevation: 6,
-          child: Row(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.purple, width: 3)),
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  '\$${trans.amount}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.purple,
+    return Container(
+      height: 400,
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+            elevation: 6,
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.purple, width: 3)),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    '\$${transactions[index].amount.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
+                    ),
                   ),
                 ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    trans.title.toString(),
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    DateFormat.yMMMd().format(trans.date),
-                    style: TextStyle(color: Colors.grey),
-                  )
-                ],
-              )
-            ],
-          ),
-        );
-      }).toList(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transactions[index].title.toString(),
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      DateFormat.yMMMd().format(transactions[index].date),
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
+      ),
     );
   }
 }
